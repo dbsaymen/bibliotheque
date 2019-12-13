129b4f77017b71786f4fed0888493105a611b98c
Mariam_Omrane <mariam.omrane@enis.tn>
2019-12-13 22:43:54 +0100

    changement

--- models/Emprunt.py
+++ models/Emprunt.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 
 class Emprunt(models.Model):
+    _name = "bibliotheque.emprunt"
     dateRetour = fields.Date("Date Retour")
     dateEmprunt=fields.Date("date Emprunt")
