c57bafeb1116832fc99dc84e1f2cd56a2fd38b38
Aymen Ben Salem <dbs.aymen@gmail.com>
2019-12-14 23:35:27 +0100

    update emprunt

--- models/Emprunt.py
+++ models/Emprunt.py
@@ -2,10 +2,10 @@ from odoo import models, fields, api
 
 class Emprunt(models.Model):
     _name = "bibliotheque.emprunt"
-    dateRetour = fields.Datetime("Date Retour")
-    dateEmprunt=fields.Datetime("Date Emprunt", default=fields.datetime.now())
-    adherentId = fields.Many2one(comodel_name='bibliotheque.adherent', string="Adherent",required=True)
-    livreId = fields.Many2one(comodel_name='bibliotheque.livre',string="livre",required=True)
+    dateEmprunt=fields.Datetime("Date", default=fields.datetime.now())
+    type_empr = fields.Selection([('retour', 'Retour'), ('emprunt', 'Emprunt')])
+    adherentId = fields.Many2one(comodel_name='bibliotheque.adherent', string="Adherent", required=True)
+    livreId = fields.Many2one(comodel_name='bibliotheque.livre', string="livre", required=True)
     def name_get(self):
         result=[]
         for Emprunt in self:
