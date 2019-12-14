833fb195b2684a69c8237d7f0974d2244383e54d
Mariam_Omrane <mariam.omrane@enis.tn>
2019-12-14 23:37:18 +0100

    numLivreDesponible

--- models/Livre.py
+++ models/Livre.py
@@ -6,12 +6,15 @@ class Livre (models.Model):
     titre = fields.Char('Titre')
     auteurId= fields.Many2one(comodel_name="bibliotheque.auteur",string="Auteur")
     numLivres = fields.Integer(string="nombre des livres")
-    numLivresDisponibles = fields.Integer(string="nombre des livres disponibles")
+    numLivresDisponibles = fields.Integer(string="nombre des livres disponibles", compute="comp_livre_dispo")
     empruntIds = fields.One2many(comodel_name='bibliotheque.emprunt',
                                  inverse_name='livreId',string="Emprint")
     def name_get(self):
         result=[]
-        for Livre in self:
-            name="["+Livre.auteurId.f_name+"]"+Livre.titre
-            result.append((Livre.id,name))
+        for livre in self:
+            name="["+Livre.auteurId.f_name+"]"+livre.titre
+            result.append((livre.id,name))
         return result
+
+    def comp_livre_dispo(self):
+        self.numLivresDisponibles = self.numLivres-len(self.empruntIds)
